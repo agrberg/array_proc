6d4d8c5144977881b8e6f59eb270911c6d0626ce
Aaron Rosenberg <aarongrosenberg@gmail.com>
2014-01-31 16:07:20 -0500

    Update spec

--- spec/array_proc_spec.rb
+++ spec/array_proc_spec.rb
@@ -7,20 +7,21 @@ describe Array do
     end
 
     it 'should turn all items in the array to procs' do
-      mock_obj = mock(Object)
-      mock_obj.should_receive(:some_method)
-      [:some_method].to_proc.call(mock_obj)
+      double_obj = double(Object)
+      double_obj.should_receive(:some_method)
+
+      [:some_method].to_proc.call(double_obj)
     end
 
     it 'should return an array of the results of procs' do
       proc = [:empty?, :to_i].to_proc
-      proc.call('5').should ==([false, 5])
+      proc.call('5').should == [false, 5]
     end
 
     it 'should be usable in a map as an & proc to see the objects' do
-      mock_1 = mock(:method_1 => 5, :method_2 => 10)
-      mock_2 = mock(:method_1 => 1, :method_2 => 2)
-      [mock_1, mock_2].map(&[:method_1, :method_2]).should ==([[5, 10], [1, 2]])
+      double_1 = double(:method_1 => 5, :method_2 => 10)
+      double_2 = double(:method_1 => 1, :method_2 => 2)
+      [double_1, double_2].map(&[:method_1, :method_2]).should == [[5, 10], [1, 2]]
     end
   end
 end
